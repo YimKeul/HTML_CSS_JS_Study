e2c8e8c410cb27d1218e098722d97ec89e7a53de
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-07-05 02:15:28 +0900

    add function

--- vanilaJS/app.js
+++ vanilaJS/app.js
@@ -1,5 +1,11 @@
 const canvas = document.getElementById("jsCanvas")
 
+let painting = false
+
+function stopPainting () {
+  painting = false;
+}
+
 function onMouseMove(event) {
     //확인용 
     //console.log(event)
@@ -9,8 +15,25 @@ function onMouseMove(event) {
     //console.log(x , y)
 }
 
+//마우스 클릭
+function onMouseDown (event) {
+  
+  painting = true
+}
+
+//마우스 클릭 해제
+function onMouseUp (event) {
+  stopPainting()
+}
+
+
+function onMouseLeave (event){
+  stopPainting()
+} 
+
 if(canvas){
-  
   canvas.addEventListener("mousemove" ,onMouseMove)//마우스 포인터 동작확인
-  
+  canvas.addEventListener("mousedown", onMouseDown)
+  canvas.addEventListener("mouseup" , onMouseUp)
+  canvas.addEventListener("mouseleave" , stopPainting)
 }
