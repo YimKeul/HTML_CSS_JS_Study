69e38ab12855169166d9496783d8c4628f8430d1
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-07-05 16:05:52 +0900

    css 수정

--- vanilaJS/app.js
+++ vanilaJS/app.js
@@ -3,14 +3,15 @@ const ctx = canvas.getContext("2d")
 const color = document.getElementsByClassName("jsColor")
 const range = document.getElementById("jsRange")
 const mode = document.getElementById("jsMode")
-
+const saveBtn = document.getElementById("jsSave")
 const INITIAL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 700
 
 canvas.width = CANVAS_SIZE
 canvas.height = CANVAS_SIZE
 
-
+ctx.fillStyle = "white"
+ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE)
 ctx.strokeStyle =  INITIAL_COLOR
 ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5
@@ -77,12 +78,26 @@ function handleCanvasClick(){
   }
 }
 
+function handleCM (event){
+  
+  event.preventDefault()
+ 
+}
+function handleSaveClick(){
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[🎨]";
+  link.click();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove) //마우스 포인터 동작확인
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
     canvas.addEventListener("click" , handleCanvasClick)
+    canvas.addEventListener("contextmenu", handleCM)
 }
 
 //console.log(Array.from(color))
@@ -97,4 +112,8 @@ if (range) {
 }
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+  saveBtn.addEventListener("click" , handleSaveClick)
 }
