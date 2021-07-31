5df692bab9052eaf57007ad631db3cce7c53ea03
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-07-31 18:25:18 +0900

    edit Nav

--- PORTFOLIO/main.js
+++ PORTFOLIO/main.js
@@ -1,8 +1,11 @@
 const toogleBTN = document.querySelector('.navbar_toogleBTN');
 const menu = document.querySelector('.navbar_menu');
-
+const nav = document.querySelector('.navbar');
+const logo = document.querySelector('.navbar_logo i');
 
 toogleBTN.addEventListener('click', ()=>{
   menu.classList.toggle('active');
+  nav.classList.toggle('active');
+  logo.classList.toggle('active');
 });
 
