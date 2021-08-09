9b7f2f6631ab06128d461b94f9cfe7d51050d251
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-08-09 15:05:46 +0900

    edit Projcet Section

--- PORTFOLIO/main.js
+++ PORTFOLIO/main.js
@@ -17,64 +17,57 @@ const about = document.querySelector(".n_m_a");
 const skill = document.querySelector(".n_m_s");
 const project = document.querySelector(".n_m_p");
 
-
-
 const c_i = document.querySelector(".INTRO");
 const c_a = document.querySelector(".PERSONAL");
 const c_s = document.querySelector(".SKILL");
 const c_p = document.querySelector(".PROJECT");
 
 profile.addEventListener("click", () =>
-  window.scrollTo({ top: c_a.offsetTop-200, behavior: "smooth" })
+  window.scrollTo({ top: c_a.offsetTop - 200, behavior: "smooth" })
 );
-
-
 about.addEventListener("click", () =>
   window.scrollTo({ top: c_a.offsetTop - 200, behavior: "smooth" })
 );
 skill.addEventListener("click", () =>
-  window.scrollTo({ top: c_s.offsetTop , behavior: "smooth" })
+  window.scrollTo({ top: c_s.offsetTop, behavior: "smooth" })
 );
 
 project.addEventListener("click", () =>
-window.scrollTo({ top: c_p.offsetTop - 100, behavior: "smooth" })
+  window.scrollTo({ top: c_p.offsetTop, behavior: "smooth" })
 );
 
 
 
+function open_chatroom() {
+  var windowWidth = $(window).width();
+  if (windowWidth < 575) {
+    //창 가로 크기가 500 미만일 경우
+    profile.addEventListener("click", () =>
+      window.scrollTo({ top: c_a.offsetTop, behavior: "smooth" })
+    );
 
+    about.addEventListener("click", () =>
+      window.scrollTo({ top: c_a.offsetTop, behavior: "smooth" })
+    );
 
-function open_chatroom(){
-  var windowWidth = $( window ).width();
-  if(windowWidth < 575) {
-  //창 가로 크기가 500 미만일 경우
-  profile.addEventListener("click", () =>
-  window.scrollTo({ top: c_a.offsetTop, behavior: "smooth" })
-);
-
-  $("#remote").css({"display" :"none"});
+    $("#remote").css({ display: "none" });
   } else {
-  //창 가로 크기가 500보다 클 경우
- 
+    //창 가로 크기가 500보다 클 경우
   }
-  }
-  
- 
+}
 
 $(document).ready(function () {
   /*스크롤 내릴때 어바웃 스킬 막대바 애니메이션 */
   $(window).scroll(function () {
-
     /*오른쪽 메뉴가 어바웃부터 나타남*/
 
-    if ($(this).scrollTop() > 700){
-      $("#remote").css({"display" :"block"});
+    if ($(this).scrollTop() > 700) {
+      $("#remote").css({ display: "block" });
     } else {
-      $("#remote").css({"display" :"none"});
+      $("#remote").css({ display: "none" });
     }
     open_chatroom();
     /*오른쪽 메뉴 스크롤*/
-   
 
     if ($(this).scrollTop() > 600) {
       $("#list2").css("background-color", "#e5e5e5");
@@ -86,7 +79,6 @@ $(document).ready(function () {
       $("#list2").css("background-color", "transparent");
     }
 
- 
     if ($(this).scrollTop() > 1200) {
       $("#list3").css("background-color", "#e5e5e5");
     } else {
@@ -96,8 +88,6 @@ $(document).ready(function () {
     if ($(this).scrollTop() > 2800) {
       $("#list3").css("background-color", "transparent");
     }
-
- 
 
     if ($(this).scrollTop() > 2900) {
       $("#list4").css("background-color", "#e5e5e5");
@@ -110,4 +100,3 @@ $(document).ready(function () {
     }
   });
 });
-
